b32b1cce78266b68c31dc5dd7d5b70c33b7124d4
Seanachan <hsinchen.pai1218@gmail.com>
2025-12-10 20:36:06 +0800

    1210 last update

--- welcom_onboard.X/bluetooth.c
+++ welcom_onboard.X/bluetooth.c
@@ -1,57 +1,57 @@
-// CONFIG1H
-#pragma config OSC = INTIO67 // Oscillator Selection bits (HS oscillator)
-#pragma config FCMEN = OFF   // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
-#pragma config IESO = ON     // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+// // CONFIG1H
+// #pragma config OSC = INTIO67 // Oscillator Selection bits (HS oscillator)
+// #pragma config FCMEN = OFF   // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+// #pragma config IESO = ON     // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
 
-// CONFIG2L
-#pragma config PWRT = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config BOREN = SBORDIS // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
-#pragma config BORV = 3        // Brown Out Reset Voltage bits (Minimum setting)
+// // CONFIG2L
+// #pragma config PWRT = OFF      // Power-up Timer Enable bit (PWRT disabled)
+// #pragma config BOREN = SBORDIS // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
+// #pragma config BORV = 3        // Brown Out Reset Voltage bits (Minimum setting)
 
-// CONFIG2H
-#pragma config WDT = OFF // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
-#pragma config WDTPS = 1 // Watchdog Timer Postscale Select bits (1:1)
+// // CONFIG2H
+// #pragma config WDT = OFF // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+// #pragma config WDTPS = 1 // Watchdog Timer Postscale Select bits (1:1)
 
-// CONFIG3H
-#pragma config CCP2MX = PORTC // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
-#pragma config PBADEN = ON    // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
-#pragma config LPT1OSC = OFF  // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
-#pragma config MCLRE = ON     // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+// // CONFIG3H
+// #pragma config CCP2MX = PORTC // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+// #pragma config PBADEN = ON    // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+// #pragma config LPT1OSC = OFF  // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+// #pragma config MCLRE = ON     // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
 
-// CONFIG4L
-#pragma config STVREN = ON // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-#pragma config LVP = OFF   // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
-#pragma config XINST = OFF // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+// // CONFIG4L
+// #pragma config STVREN = ON // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+// #pragma config LVP = OFF   // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+// #pragma config XINST = OFF // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
 
-// CONFIG5L
-#pragma config CP0 = OFF // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
-#pragma config CP1 = OFF // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
-#pragma config CP2 = OFF // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
-#pragma config CP3 = OFF // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+// // CONFIG5L
+// #pragma config CP0 = OFF // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+// #pragma config CP1 = OFF // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+// #pragma config CP2 = OFF // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+// #pragma config CP3 = OFF // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
 
-// CONFIG5H
-#pragma config CPB = OFF // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
-#pragma config CPD = OFF // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+// // CONFIG5H
+// #pragma config CPB = OFF // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+// #pragma config CPD = OFF // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
 
-// CONFIG6L
-#pragma config WRT0 = OFF // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
-#pragma config WRT1 = OFF // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
-#pragma config WRT2 = OFF // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
-#pragma config WRT3 = OFF // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+// // CONFIG6L
+// #pragma config WRT0 = OFF // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+// #pragma config WRT1 = OFF // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+// #pragma config WRT2 = OFF // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+// #pragma config WRT3 = OFF // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
 
-// CONFIG6H
-#pragma config WRTC = OFF // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
-#pragma config WRTB = OFF // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
-#pragma config WRTD = OFF // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+// // CONFIG6H
+// #pragma config WRTC = OFF // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+// #pragma config WRTB = OFF // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+// #pragma config WRTD = OFF // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
 
-// CONFIG7L
-#pragma config EBTR0 = OFF // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
-#pragma config EBTR1 = OFF // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
-#pragma config EBTR2 = OFF // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
-#pragma config EBTR3 = OFF // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+// // CONFIG7L
+// #pragma config EBTR0 = OFF // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+// #pragma config EBTR1 = OFF // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+// #pragma config EBTR2 = OFF // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+// #pragma config EBTR3 = OFF // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
 
-// CONFIG7H
-#pragma config EBTRB = OFF // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+// // CONFIG7H
+// #pragma config EBTRB = OFF // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
 
 #include <ctype.h>
 // #include <pic18f4520.h>
@@ -180,24 +180,6 @@ int GetString(char *str)
     }
 }
 
-// void __interrupt(low_priority) Lo_ISR(void)
-//{
-//     if (RCIF)
-//     {
-//         if (RCSTAbits.OERR)
-//         {
-//             CREN = 0;
-//             Nop();
-//             CREN = 1;
-//         }
-//
-//         MyusartRead();
-//     }
-//
-//     // process other interrupt sources here, if required
-//     return;
-// }
-
 // ---------------- Settings --------------------
 
 void Initialize(void)
@@ -251,8 +233,6 @@ void Initialize(void)
     ADCON1 = 0x0F; // Set all pins to Digital Mode
     LATBbits.LATB4 = 1;
     TRISBbits.TRISB4 = 0; // Make RB4 Output
-    // Start ADC conversion
-    // ADCON0bits.GO = 1;
 }
 
 // ---------------- OOP --------------------
@@ -285,6 +265,8 @@ void keyboard_input(char *str)
     for (int i = 0; i < strlen(str); i++)
         str[i] = toupper(str[i]);
     // printf("BT CMD: %s\n", str);
+
+    __delay_ms(30);
 
     if (strcmp(str, "FORWARD") == 0)
     {
@@ -335,21 +317,5 @@ void keyboard_input(char *str)
     {
         // printf("Unknown CMD: %s\n", str);
     }
-    __delay_ms(100);
+    __delay_ms(50);
 }
-
-// void main()
-//{
-//     Initialize();
-//     DF_Init();
-//     // printf("PIC ready\r\n");
-//     char str[STR_MAX];
-//
-//     DF_PlayTrack1();
-//
-//     while (1)
-//     {
-//         if (GetString(str))
-//             keyboard_input(str);
-//     }
-// }
